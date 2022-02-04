7f7e74e1ca7939b0fb43646f482c6350e7ebfab4
Ernest Ibeh <ibeh_ernest@yahoo.com>
2022-02-04 19:58:21 +0100

    Done with landing page

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -3,13 +3,74 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
+  socials = [
+    {
+      icon: '../../../../assets/images/twitter.svg',
+      url: 'https://twitter.com',
+    },
+    {
+      icon: '../../../../assets/images/instagram.svg',
+      url: 'https://instagram.com',
+    },
+    {
+      icon: '../../../../assets/images/linkedin.svg',
+      url: 'https://linkedin.com',
+    },
+  ];
 
-  constructor() { }
+  linksOne = [
+    {
+      title: 'About',
+      url: '/about',
+    },
+    {
+      title: 'Private Coaching',
+      url: '/private-coaching',
+    },
+    {
+      title: 'Membership',
+      url: '/membership',
+    },
+    {
+      title: 'Podcast',
+      url: '/podcast',
+    },
+  ];
 
-  ngOnInit(): void {
-  }
+  linksTwo = [
+    {
+      title: 'Media',
+      url: '/media',
+    },
+    {
+      title: 'Contact',
+      url: '/contact-me',
+    },
+    {
+      title: 'Privacy Policy',
+      url: '/privacy-policy',
+    },
+    {
+      title: 'Terms & Conditions',
+      url: '/terms-conditions',
+    },
+  ];
 
+  contacts = [
+    {
+      url: 'lauren@gmail.com',
+      title: 'Lauren@gmail.com',
+      icon: 'ri-send-plane-fill',
+    },
+    { url: '+19348903468', title: '+1 934 890 3468', icon: 'ri-phone-fill' },
+  ];
+
+  year = new Date().getFullYear();
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
